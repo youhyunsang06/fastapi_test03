b4bbe6c5ecdd7a0989817a6f3d76f92e793540e4
youhyunsang <delipure@naver.com>
2026-04-21 11:06:26 +0900

    글 수정반영 기능 구현함

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def delete(num: int, db: Session = Depends(get_db)): # 경로 변수의 이름�
 
 
 @app.get("/post/edit/{num}")
-def edit(num: int, request: Request, db: Session = Depends(get_db)):
+def editForm(num: int, request: Request, db: Session = Depends(get_db)):
     # 수정할 글정보를 읽어오기 위한 query 작성
     query = text("""
         SELECT num, writer, title, content, created_at
@@ -108,5 +108,24 @@ def edit(num: int, request: Request, db: Session = Depends(get_db)):
         name="post/edit.html",
         context={
             "post":row
+        }
+    )
+
+# 글 수정 반영 
+@app.post("/post/edit/{num}")
+def edit(request: Request, num: int, title: str = Form(...), content: str = Form(...), db: Session = Depends(get_db)):
+    query = text("""
+        UPDATE post
+        SET title=:title, content=:content
+        WHERE num=:num
+    """)
+    db.execute(query, {"num":num, "title":title, "content":content})
+    db.commit()
+    return templates.TemplateResponse(
+        request=request, 
+        name="post/alert.html",
+        context={
+            "msg":"글 정보를 수정 했습니다!",
+            "url":"/post"
         }
     )
